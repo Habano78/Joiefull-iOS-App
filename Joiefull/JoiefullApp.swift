94cbb48265be099acf53ca8733009116e84154ec
William-Gabriel <willx_gabriel@yahoo.com>
2025-11-03 11:10:00 +0100

    Initial Commit

---
+++ Joiefull/JoiefullApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoiefullApp.swift
+//  Joiefull
+//
+//  Created by Perez William on 03/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct JoiefullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
